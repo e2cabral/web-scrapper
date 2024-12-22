843fe6781268f812d01e6b2dfe21ce2a224b23e9
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-12-21 21:51:39 -0300

    refactor: change scraping logic to use reusable utility functions
    Replaced inline Cheerio parsing with `getElementText` and `getElementAttribute` utility functions for improved code reusability and readability. This simplifies the handling of element extraction and reduces repetitive code patterns.

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,18 @@
-import * as cheerio from 'cheerio';
 import {httpClient, URL} from "./infra/http";
+import {getElementAttribute, getElementText} from "./infra/scrapper";
 
 const app = async () => {
   const response = await httpClient(URL.cnnBrasil).get('/economia');
 
-  const titles = [];
-  const imgs = [];
-
-  const $ = cheerio.load(response.data);
-
-  $('h3.block__news__title').each((i, el) => {
-    titles.push($(el).text());
-  });
-
-  $('figure.block__news__thumb > img').each((i, el) => {
-    imgs.push($(el).attr('src'));
-  });
+  const titles = getElementText<string>('h3.block__news__title', response.data);
+  const images = getElementAttribute<string>(
+    'figure.block__news__thumb > img',
+    response.data,
+    'src'
+  );
 
   console.log('#### TITLES:', titles);
-  console.log('#### IMGS:', imgs);
+  console.log('#### IMGS:', images);
 }
 
 app().then();
