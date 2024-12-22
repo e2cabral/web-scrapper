aa11b9dca244cb997952be38dc2506085e0f34db
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-12-21 21:11:31 -0300

    feat: add web scraping for titles and images using Axios and Cheerio
    Replaced the basic "Hello World" log with a web scraper that fetches economic news titles and image URLs from a CNN Brasil page. Integrated Axios for HTTP requests and Cheerio for HTML parsing. Results are logged for validation purposes.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
-const main = () => {
-  console.log('Hello World');
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+const main = async () => {
+  const response = await axios.get('https://www.cnnbrasil.com.br/economia/');
+
+  const titles = [];
+  const imgs = [];
+
+  const $ = cheerio.load(response.data);
+
+  $('h3.block__news__title').each((i, el) => {
+    titles.push($(el).text());
+  });
+
+  $('figure.block__news__thumb > img').each((i, el) => {
+    imgs.push($(el).attr('src'));
+  });
+
+  console.log('#### TITLES:', titles);
+  console.log('#### IMGS:', imgs);
 }
 
-main();
+main().then();
